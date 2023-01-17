da24e37d3eb8d97a681a50988586417d43af1678
ugoingnowhereboy <ugnw20@gmail.com>
2023-01-17 16:24:45 +0300

    Outdated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Container, Text,  Grid } from "@nextui-org/react";
 import Accounts from './Comps/Accounts';
 import Footer from './Comps/Footer';
 import Cards from './Comps/Cards';
+import Link from 'react-scroll/modules/components/Link';
 
 
 
@@ -21,7 +22,10 @@ const App = () => {
         <Nav />
 
         <Text css={{marginBottom:'$6', marginTop:'$24', textAlign:'center', '@xsMax':{fontSize:'26px'}, '@xs':{fontSize:'50px'}}}>
-          Eyad's Portfolio/Resume</Text>
+          (OUTDATED) <a href='https://eyadsportfolio.vercel.app'>Go to my new portfolio<br/>https://eyadsportfolio.vercel.app </a></Text>
+          <Text>
+           
+          </Text>
 
         <Cards />
 
